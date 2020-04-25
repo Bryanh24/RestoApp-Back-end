2207ccc0e46b69377ec6f34fdb3bd21788455953
Bryan Hernandez <bryanh24@hotmail.com>
2020-04-24 18:53:02 -0600

    solucionar puerto

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use('/api',apiRouter);
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT||3000,()=>{
     console.log('servidor arrancado');
 });
